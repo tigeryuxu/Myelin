4035035c946668e9fa6e72917c675b1e21da80f7
ChitsaD <daryan.chitsaz@mail.mcgill.ca>
2019-04-22 18:57:03 -0400

    Hopefully-Usable Functions for Scrambling Training Data
    - i am now replacing version 4 which had glaring errors but i think they are fixed, i did not test everything here but the functions seem to be behaving as expected (and I tested everything fairly exhaustively before putting it in function form and it all worked so we should be good)

--- CNN-UNet/Training/190415M_UnetTrainingFxn_0v5_actualFunctions.py
+++ CNN-UNet/Training/190415M_UnetTrainingFxn_0v5_actualFunctions.py
@@ -13,72 +13,57 @@ Created on Thu Apr 11 13:35:56 2019
     
 - tried to set it up so each has 2 parameters - first chooses the type (ie 
     Gaussian noise vs salt and pepper), second is the magnitude (generally btwn 0
-    and 1)
+    and 1, can be >1 for scaling / stretching)
     - only 1 type of blur for now (probably don't need more than this..) so only 1 
     parameter there for the sigma
-    - scale2 can be >1 if you want to upscale
 
-- other to-do's
-    - less stupid variable names (lol sorry) 
-    - reduce importing (did not realize you can import specific fnxs rather than whole modules)
-    - make bigger function that randomly applies these to subsets of images
 """
 
 
 import numpy as np
-import skimage as ski
-
 import skimage.io
-import scipy 
+import scipy # necessary? at this point im just scared to touch anything so it will stay
 import matplotlib.pyplot as plt
 
-from random import randint, choice
-from math import floor
-
-im = skimage.io.imread("C:/Users/darya/Desktop/JbbHeart_vf.tif")
+im = skimage.io.imread("C:/Users/darya/Desktop/test.tif")
 
 
 # rescale, 0=none 1=constant 2=random
 # second term is scale factor (can be >1), or random range of scaling (btwn 0-1)
-# side note - did wierd multiplying-then-dividing-by-10 thing so that 
+# note - did wierd multiplying-then-dividing-by-10 thing so that 
     # scale2 could be an integer for randint, then go back to being <1
-    scale1 = 2
-    scale2 = 0.5
-
-    
-def changeScale(scale1,scale2):
-    import skimage.transform
+def changeScale(im,kind,amount):
+    from skimage.transform import rescale
+    from random import randint, choice
+    from math import floor
     if scale1 == 1:
-        imScaled = skimage.transform.rescale(im,scale2,anti_aliasing=True,multichannel=True)
+        imScaled = rescale(im,scale2,anti_aliasing=True,multichannel=True)
     if scale1 ==2: 
-        imScaled = skimage.transform.rescale(im,scale2/randint(1,floor(scale2*10))/10, anti_aliasing=True,multichannel=True)
+        imScaled = rescale(im,scale2/randint(1,floor(scale2*10))/10, anti_aliasing=True,multichannel=True)
     return imScaled
+
 
 # gaussian blur
 # note - gaussian function has other optional parameters including wrap
-# did not bother making a function here as clearly it already exists and works fine
-blur = 2
-import skimage.filters
-
-imBlurred = skimage.filters.gaussian(im,sigma=blur,multichannel=True)
+def changeBlur(im,amount):
+    if blur > 0:
+        from skimage.filters import gaussian
+        imBlurred = gaussian(im,sigma=blur,multichannel=True)
+    return imBlurred
 
 
 # gaussian noise, type is first term (0=none, 1=gaussian, 2=speckle, 3=s&p)
-# ideally add other noise in future (speckle, s&p could be useful)
 # note - seems like speckle in general is best at emulating immunofluorescence noise
-noise1 = 3
-noise2 = 0.5
-
-def changeNoise(noise1,noise2):
-    import skimage.util
+def changeNoise(im,kind,amount):
+    from skimage.util import random_noise
     noise2 = noise2*0.1   # was WAY too strong so in interest of keeping things consistent
                           # this way if noise2=0.5, 5% of pixels will become noise instead of 50                     
     if noise1 == 1:
-        imNoised = skimage.util.random_noise(im,mode="gaussian",var=noise2)
+        imNoised = random_noise(im,mode="gaussian",var=noise2)
     if noise1 == 2:
-        imNoised = skimage.util.random_noise(im,mode="speckle",var=noise2)
+        imNoised = random_noise(im,mode="speckle",var=noise2)
     if noise1 == 3:
-        imNoised = skimage.util.random_noise(im,mode="s&p",amount=noise2)
+        imNoised = random_noise(im,mode="s&p",amount=noise2)
     return imNoised
 
 
@@ -87,10 +72,7 @@ def changeNoise(noise1,noise2):
     # image can shift up by up by 5% and down by up to 5%; 50% means 25% on each end
 # should rewrite random to increase OR decrease floor and ceiling, as was done with stretch below
 # should also allow channel(s) to be specified; does all flattened channels now
-cont1 = 3
-cont2 = 0.5
-
-def changeContrast(cont1,cont2):
+def changeContrast(im,kind,amount):
     import skimage.exposure
     if cont1 == 1:
         imContrast = skimage.exposure.equalize_hist(im)
@@ -100,6 +82,10 @@ def changeContrast(cont1,cont2):
         imMin = im.min(axis=0).min()
         imMax = im.max(axis=0).max()
         imRange = imMax-imMin
+        
+        # this takes a percentage of the total image range to add to floor or subtract from ceiling
+        # kind of stupid but i think the right way to randomly adjust intensities
+        
         newMin = imMin + randint(0,floor(cont2*imRange/2))
         newMax = imMax - randint(0,floor(cont2*imRange/2))
         imContrast = skimage.exposure.rescale_intensity(im,in_range='image',out_range=(newMin.astype(int),newMax.astype(int)))
@@ -107,33 +93,27 @@ def changeContrast(cont1,cont2):
 
 # stretch image (0 = none, 1 = X dimension only, 2 = Y only, 3 = both)
 # second term is randomness magnitude (btwn 0 and 1) ie if stretch2 = 0.5, then
-    # images will be stretched up by 50%
+    # images will be stretched up by anywhere from 0 to 50%
 # may need to add in something to reshape image to original size after...
-stretch1 = 3
-stretch2 = 0.5
-    
-def changeStretch(stretch1,stretch2):
-    import skimage.transform
+def changeStretch(im,kind,amount):
+    from skimage.transform import resize
     if stretch1 == 1:
         if choice([True, False]) == True:           # randomly decides if neg or pos stretching
-            imStretch = skimage.transform.resize(im,(im.shape[0]*stretch2,im.shape[1]))
+            imStretch = resize(im,(im.shape[0]*stretch2,im.shape[1]))
         else:
-            imStretch = skimage.transform.resize(im,(im.shape[0]/stretch2,im.shape[1]))
-            
+            imStretch = resize(im,(im.shape[0]/stretch2,im.shape[1]))        
     if stretch1 == 2:
         if choice([True, False]) == True:         
-            imStretch = skimage.transform.resize(im,(im.shape[1],im.shape[1]*stretch2))
+            imStretch = resize(im,(im.shape[1],im.shape[1]*stretch2))
         else:
-            imStretch = skimage.transform.resize(im,(im.shape[1],im.shape[1]/stretch2))
-            
+            imStretch = resize(im,(im.shape[1],im.shape[1]/stretch2))
     if stretch1 == 3:
         if choice([True, False]) == True:   
-            imStretch = skimage.transform.resize(im,(im.shape[0]*stretch2,im.shape[1]))
+            imStretch = resize(im,(im.shape[0]*stretch2,im.shape[1]))
         else:
-            imStretch = skimage.transform.resize(im,(im.shape[0]/stretch2,im.shape[1]))
-            
+            imStretch = resize(im,(im.shape[0]/stretch2,im.shape[1]))
         if choice([True, False]) == True:           
-            imStretch = skimage.transform.resize(im,(im.shape[0],im.shape[1]*stretch2))
+            imStretch = resize(im,(im.shape[0],im.shape[1]*stretch2))
         else:
-            imStretch = skimage.transform.resize(im,(im.shape[0],im.shape[1]/stretch2))
+            imStretch = resize(im,(im.shape[0],im.shape[1]/stretch2))
     return imStretch
